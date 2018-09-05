79a7ccdbd14d20a4dc85162f334de9679de7991e
Adam Ryman <adamryman@gmail.com>
2018-09-05 15:43:14 -0700

    Add benchmarks and compare against stdlib quicksort

--- README.md
+++ README.md
@@ -9,10 +9,45 @@ This is just for fun, maybe it could be useful to someone.
 - https://en.wikipedia.org/wiki/Counting_sort
 - https://www.geeksforgeeks.org/counting-sort/
 
+## Benchmarks (run them yourself, this is from a noisy laptop)
+
+```
+$ go test -bench=.
+goos: linux
+goarch: amd64
+pkg: github.com/adamryman/counting
+BenchmarkSortStdlib1-4                  10000000               105 ns/op
+BenchmarkSort1-4                        20000000               130 ns/op
+BenchmarkSortStdlib2-4                  10000000               260 ns/op
+BenchmarkSort2-4                         5000000               219 ns/op
+BenchmarkSortStdlib3-4                   5000000               261 ns/op
+BenchmarkSort3-4                         5000000               246 ns/op
+BenchmarkSortStdlib10-4                  2000000               796 ns/op
+BenchmarkSort10-4                        2000000               583 ns/op
+BenchmarkSortStdlib20-4                  1000000              1810 ns/op
+BenchmarkSort20-4                        1000000              1129 ns/op
+BenchmarkSortStdlib40-4                   300000              3863 ns/op
+BenchmarkSort40-4                        1000000              2010 ns/op
+BenchmarkSort10And1000Stdlib-4             20000             83303 ns/op
+BenchmarkSort10And1000-4                   30000             41653 ns/op
+BenchmarkSort10And10000Stdlib-4             2000            865794 ns/op
+BenchmarkSort10And10000-4                   5000            409069 ns/op
+BenchmarkSort10And100000Stdlib-4             200           8699055 ns/op
+BenchmarkSort10And100000-4                   500           4333820 ns/op
+BenchmarkSort10And1000000Stdlib-4             20          88007947 ns/op
+BenchmarkSort10And1000000-4                   30          40521176 ns/op
+BenchmarkSort100And1000000Stdlib-4            10         110538889 ns/op
+BenchmarkSort100And1000000-4                  30          34794485 ns/op
+BenchmarkSort1000And1000000Stdlib-4           10         141395959 ns/op
+BenchmarkSort1000And1000000-4                 50          42218434 ns/op
+PASS
+ok      github.com/adamryman/counting   45.110s
+```
+
 ## TODO
 
 - [X] Implement counting sort
 - [ ] Add useful comments
 - [X] Add a test
 - [ ] Add multiple tests
-- [ ] Add benchmarks
+- [X] Add benchmarks
--- sort_test.go
+++ sort_test.go
@@ -1,6 +1,8 @@
 package counting
 
 import "testing"
+import "math/rand"
+import "sort"
 
 func TestSort(t *testing.T) {
 	tests := []struct {
@@ -29,3 +31,59 @@ func TestSort(t *testing.T) {
 		}
 	}
 }
+
+func RandomIntSlice(maxSize, maxLength int) []int {
+	out := make([]int, maxLength, maxLength)
+	for i, _ := range out {
+		out[i] = rand.Intn(maxSize)
+	}
+	return out
+}
+
+func benchmarkSort(maxSize, maxLength int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Sort(RandomIntSlice(maxSize, maxLength), maxSize)
+	}
+}
+
+func benchmarkSortStdlib(maxSize, maxLength int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		sort.Ints(RandomIntSlice(maxSize, maxLength))
+	}
+}
+
+func BenchmarkSortStdlib1(b *testing.B) { benchmarkSortStdlib(1, 1, b) }
+func BenchmarkSort1(b *testing.B)       { benchmarkSort(1, 1, b) }
+
+func BenchmarkSortStdlib2(b *testing.B) { benchmarkSortStdlib(2, 2, b) }
+func BenchmarkSort2(b *testing.B)       { benchmarkSort(2, 2, b) }
+
+func BenchmarkSortStdlib3(b *testing.B) { benchmarkSortStdlib(3, 3, b) }
+func BenchmarkSort3(b *testing.B)       { benchmarkSort(3, 3, b) }
+
+func BenchmarkSortStdlib10(b *testing.B) { benchmarkSortStdlib(10, 10, b) }
+func BenchmarkSort10(b *testing.B)       { benchmarkSort(10, 10, b) }
+
+func BenchmarkSortStdlib20(b *testing.B) { benchmarkSortStdlib(20, 20, b) }
+func BenchmarkSort20(b *testing.B)       { benchmarkSort(20, 20, b) }
+
+func BenchmarkSortStdlib40(b *testing.B) { benchmarkSortStdlib(40, 40, b) }
+func BenchmarkSort40(b *testing.B)       { benchmarkSort(40, 40, b) }
+
+func BenchmarkSort10And1000Stdlib(b *testing.B) { benchmarkSortStdlib(10, 1000, b) }
+func BenchmarkSort10And1000(b *testing.B)       { benchmarkSort(10, 1000, b) }
+
+func BenchmarkSort10And10000Stdlib(b *testing.B) { benchmarkSortStdlib(10, 10000, b) }
+func BenchmarkSort10And10000(b *testing.B)       { benchmarkSort(10, 10000, b) }
+
+func BenchmarkSort10And100000Stdlib(b *testing.B) { benchmarkSortStdlib(10, 100000, b) }
+func BenchmarkSort10And100000(b *testing.B)       { benchmarkSort(10, 100000, b) }
+
+func BenchmarkSort10And1000000Stdlib(b *testing.B) { benchmarkSortStdlib(10, 1000000, b) }
+func BenchmarkSort10And1000000(b *testing.B)       { benchmarkSort(10, 1000000, b) }
+
+func BenchmarkSort100And1000000Stdlib(b *testing.B) { benchmarkSortStdlib(100, 1000000, b) }
+func BenchmarkSort100And1000000(b *testing.B)       { benchmarkSort(100, 1000000, b) }
+
+func BenchmarkSort1000And1000000Stdlib(b *testing.B) { benchmarkSortStdlib(1000, 1000000, b) }
+func BenchmarkSort1000And1000000(b *testing.B)       { benchmarkSort(1000, 1000000, b) }
